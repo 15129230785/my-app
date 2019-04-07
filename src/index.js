7be7cba54f562c09cfda8e509ede01315e784e41
evan <15129230785@163.com>
2019-04-07 22:10:17 +0800

    学习

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HelloWorld from './helloWorld';
+
+const root = document.querySelector("#root");
+
+
+ReactDOM.render(<HelloWorld name="evan">  </HelloWorld>, root);
+
